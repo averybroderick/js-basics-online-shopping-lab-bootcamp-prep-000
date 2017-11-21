9ddce5788b9d1d6207183e44c5544d8540318540
Avery Broderick <avery.c.broderick@gmail.com>
2017-11-21 20:11:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function addToCart(item) {
   var itemObj = {};
   itemObj[item] = Math.floor(Math.random() * 100);
   cart.push(itemObj);
-  console.log(`${item} has been added to your cart.`)
+  console.log(`${item} has been added to your cart.`);
   return cart;
 }
 
